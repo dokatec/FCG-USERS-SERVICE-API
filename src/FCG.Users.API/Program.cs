520b937e6f842b237e9dc842c2f3b670e0d58106
Jhonatan Pacheco <jhonatan.tec@gmail.com>
2026-02-19 21:01:28 -0300

    update: atualizando api

--- src/FCG.Users.API/Program.cs
+++ src/FCG.Users.API/Program.cs
@@ -26,7 +26,7 @@ builder.Services.AddEndpointsApiExplorer();
 // --- Banco de Dados ---
 // Altere de UseNpgsql para UseSqlServer
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
 
 // Configuração do MassTransit
